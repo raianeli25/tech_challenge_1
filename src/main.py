bfe1b0943bf3779678149235a323b509b62e6cd0
Chrystian Remes <chrystian@windowslive.com>
2024-05-16 09:31:10 -0300

    moved auth to auth folder

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from web_scrapping import get_export_import_page, get_production_commercializati
 from classes import ModelExport, ModelImport, ModelProcessing, Token
 from fastapi import FastAPI, Path, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from auth import *
+from auth.auth import *
 
 app = FastAPI()
 
